3596bba7dc07fb08540c3127519e41f46cfbb45b
Maxmaraimo <ozodbekmahmaraimov16@gmail.com>
2023-12-19 15:17:50 +0500

    small changes && updating

---
+++ empUpWork/books/views.py
@@ -0,0 +1,27 @@
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.shortcuts import redirect, render
+
+from .forms import BookForm
+from .models import Books
+
+
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST, request.FILES)
+        if form.is_valid():
+            book = form.save(commit=False)
+            book.author = request.user
+            book.save()
+            messages.success(request, 'Book added successfully!')
+            return redirect('books_view')
+    else:
+        form = BookForm()
+
+    context = {'form': form}
+    return render(request, 'add_book.html', context)
+
+
+def books(request):
+    context = {'books': Books.objects.all()}
+    return render(request, 'books.html', context)
--- empUpWork/users/views.py
+++ empUpWork/users/views.py
@@ -1,32 +1,31 @@
-from django.shortcuts import render
-
-from .forms import UserForm
-from .models import User
 from .usecases import *
 
+# def users(request):
+#     if request.method == 'POST':
+#         form = UserForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             username = form.cleaned_data['first_name']
+#             messages.success(
+#                 request,
+#                 f'{username} hass been created successfully!'
+#             )
+#             form.save(commit=True)
 
-def first_view(request):
-    if request.method == 'POST':
-        form = UserForm(request.POST)
-        if form.is_valid():
-            form.save(commit=True)
+#     context = get_users_context()
 
-    context = get_users_context()
-
-    return render(request, 'index.html', context)
-
-
-def user_details(request, user_id):
-    user = User.objects.get(id=user_id)
-    context = {'user': user}
-    return render(request, 'user_details.html', context)
+#     return render(request, 'index.html', context)
 
 
-def delete_user(request, del_id):
-    user = User.objects.get(id=del_id)
-    print(user.first_name)
-    user.delete()
-    print(user.first_name)
+# def user_details(request, user_id):
+#     user = User.objects.get(id=user_id)
+#     context = {'user': user}
+#     return render(request, 'user_details.html', context)
 
-    context = get_users_context()
-    return render(request, 'index.html', context)
+
+# def delete_user(request, del_id):
+#     user = User.objects.get(id=del_id)
+#     print(user.first_name)
+#     user.delete()
+#     print(user.first_name)
+
+#     return redirect('users_view')
