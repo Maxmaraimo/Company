5ea2875f0943c854fbc7b762a5e14e0a6d6f080d
Maxmaraimo <ozodbekmahmaraimov16@gmail.com>
2023-12-19 21:09:22 +0500

    Login & out & UpdateView, AddView, CreateView

--- empUpWork/books/urls.py
+++ empUpWork/books/urls.py
@@ -3,8 +3,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("", books, name="books_view"),
-    path("add_book", add_book, name="add_book"),
-    # path("details/<int:book_id>", book_details, name="book_details"),
-    # path("delete/<int:del_id>", delete_book, name="delete_book")
+    path("", BooksListView.as_view(), name="books_view"),
+    path("add_book", AddBookView.as_view(), name="add_book"),
+    path("update_book/<int:pk>", BookUpdateView.as_view(), name="update_book"),
+    path("book_details/<int:pk>", BookDetailsView.as_view(), name="book_details"),
+    path("delete_book/<int:book_id>", delete_book, name="delete_book")
 ]
--- empUpWork/users/views.py
+++ empUpWork/users/views.py
@@ -1,31 +1,58 @@
+import logging
+from math import e
+
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+
+from .forms import ProfileForm, UserForm, UserUpdateForm
 from .usecases import *
 
-# def users(request):
-#     if request.method == 'POST':
-#         form = UserForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             username = form.cleaned_data['first_name']
-#             messages.success(
-#                 request,
-#                 f'{username} hass been created successfully!'
-#             )
-#             form.save(commit=True)
-
-#     context = get_users_context()
-
-#     return render(request, 'index.html', context)
+log = logging.getLogger(__name__)
 
 
-# def user_details(request, user_id):
-#     user = User.objects.get(id=user_id)
-#     context = {'user': user}
-#     return render(request, 'user_details.html', context)
+def registration(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            messages.success(request,
+                             f"Welcome to our site {username}!")
+            form.save(commit=True)
+            return redirect('login')
+        else:
+            messages.error(request,
+                           "Please correct the errors below.")
+            context = {'form': form}
+            return render(request, 'registration/create_user.html', context)
+
+    context = {'form': UserForm()}
+    return render(request, 'registration/create_user.html', context)
 
 
-# def delete_user(request, del_id):
-#     user = User.objects.get(id=del_id)
-#     print(user.first_name)
-#     user.delete()
-#     print(user.first_name)
+@login_required
+def profile(request):
 
-#     return redirect('users_view')
+    if request.method == 'POST':
+        p_form = ProfileForm(request.POST,
+                             instance=request.user.profile,
+                             files=request.FILES)
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        if p_form.is_valid() and u_form.is_valid():
+            user = u_form.save(commit=True)
+            profile = p_form.save(commit=True)
+            messages.success(request,
+                             f"Your account has been updated!")
+            return redirect('profile')
+        else:
+            context = {
+                "u_form": u_form,
+                "p_form": p_form,
+            }
+            return render(request, 'profile.html', context)
+
+    context = {
+        'u_form': UserUpdateForm(instance=request.user),
+        'p_form': ProfileForm(instance=request.user.profile)
+    }
+    return render(request, 'profile.html', context)
