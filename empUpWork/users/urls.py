24ff7b11a8840e082d9e15236427afc83435c186
Maxmaraimo <ozodbekmahmaraimov16@gmail.com>
2023-12-19 14:38:34 +0500

    user details && delete user

--- empUpWork/users/urls.py
+++ empUpWork/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import first_view
+from .views import *
 
 urlpatterns = [
-    path("", first_view, name="first_view")
+    path("", first_view, name="first_view"),
+    path("details/<int:user_id>", user_details, name="user_details"),
+    path("delete/<int:del_id>", delete_user, name="delete_user")
 ]
