d30c9c049c4c4a990a7cf40bf219e072d6a525e0
Maxmaraimo <ozodbekmahmaraimov16@gmail.com>
2024-01-04 00:10:19 +0500

    update urls

--- empUpWork/books/urls.py
+++ empUpWork/books/urls.py
@@ -4,10 +4,10 @@ from .views import *
 
 urlpatterns = [
     path("", BooksListView.as_view(), name="books_view"),
-    path("add_book", AddBookView.as_view(), name="add_book"),
-    path("update_book/<int:pk>", BookUpdateView.as_view(), name="update_book"),
-    path("book_details/<int:pk>", BookDetailsView.as_view(), name="book_details"),
-    path("delete_book/<int:book_id>", delete_book, name="delete_book"),
+    path("add_jobs", AddBookView.as_view(), name="add_book"),
+    path("update_work/<int:pk>", BookUpdateView.as_view(), name="update_book"),
+    path("work_details/<int:pk>", BookDetailsView.as_view(), name="book_details"),
+    path("delete_work/<int:book_id>", delete_book, name="delete_book"),
     path('add_to_wishlist/<int:book_id>',
          add_to_wishlist, name="add_to_wishlist"),
 ]
