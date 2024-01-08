6180e7a48e53e52c363d2ee9d8b731b374ee55e4
Maxmaraimo <ozodbekmahmaraimov16@gmail.com>
2024-01-09 00:06:33 +0500

    random_game.py new

---
+++ python/random_game.py
@@ -0,0 +1,31 @@
+
+def game(secret_num):
+        # how to get out???
+        # one method: return False
+        # another method: use "break" and "continue" within the "while" loop
+        # Homework: get it working with while true, and no return from the function until game is over and you have printed the score
+    times = 0
+    while True:
+        guess = int(input("Enter a number please: "))
+        times += 1
+        
+        att = "attempts"
+        if times == 1:
+            att = "attempt"
+
+        if guess > secret_num:
+            if guess == secret_num + 1:
+                print("Too close, a little below than that...")
+            else:
+                print("Too high...")
+            continue
+        elif guess < secret_num:
+            if guess == secret_num - 1:
+                print("Too close, a little higher than that...")
+            else:
+                print("Too below...")
+            continue
+        else:
+            print(f"It took you {times} {att} in total...")
+            break
+    # ifyou reach here, it means you ran out of turn
