dcf783370c7f1ea2fe9a2d45403804f1c5c38be7
Maxmaraimo <ozodbekmahmaraimov16@gmail.com>
2024-01-14 00:06:20 +0500

    new func views

--- empUpWork/users/views.py
+++ empUpWork/users/views.py
@@ -138,6 +138,24 @@ def Order(request):
     context = {'form': UserForm()}
     return render(request, 'registration/temp/order.html', context)
 
+def updateProfile(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            messages.success(request,
+                             f"Welcome to our site {username}!")
+            form.save(commit=True)
+            return redirect('login')
+        else:
+            messages.error(request,
+                           "Please correct the errors below.")
+            context = {'form': form}
+            return render(request, 'registration/updateProfile.html', context)
+
+    context = {'form': UserForm()}
+    return render(request, 'registration/updateProfile.html', context) 
+
 @login_required
 def profile(request):
 
