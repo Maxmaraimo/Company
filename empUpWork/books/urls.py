1c17345b1fbf6fad6dfc55088dc6bc050976f322
Maxmaraimo <ozodbekmahmaraimov16@gmail.com>
2024-01-17 00:03:24 +0500

    update name urls

--- empUpWork/books/urls.py
+++ empUpWork/books/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("", BooksListView.as_view(), name="books_view"),
-    path("add_jobs", AddBookView.as_view(), name="add_book"),
-    path("update_work/<int:pk>", BookUpdateView.as_view(), name="update_book"),
-    path("work_details/<int:pk>", BookDetailsView.as_view(), name="book_details"),
+    path("", WorksListView.as_view(), name="books_view"),
+    path("add_jobs", AddWorkView.as_view(), name="add_book"),
+    path("update_work/<int:pk>", JobsUpdateView.as_view(), name="update_book"),
+    path("work_details/<int:pk>", WorksDetailsView.as_view(), name="book_details"),
     path("delete_work/<int:book_id>", delete_book, name="delete_book"),
     path('add_to_wishlist/<int:book_id>',
          add_to_wishlist, name="add_to_wishlist"),
