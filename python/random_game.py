994449031e39a815a156388bdd253d9e66cdcf2c
Maxmaraimo <ozodbekmahmaraimov16@gmail.com>
2024-01-25 00:01:40 +0500

    random_game

--- python/random_game.py
+++ python/random_game.py
@@ -1,9 +1,5 @@
 
 def game(secret_num):
-        # how to get out???
-        # one method: return False
-        # another method: use "break" and "continue" within the "while" loop
-        # Homework: get it working with while true, and no return from the function until game is over and you have printed the score
     times = 0
     while True:
         guess = int(input("Enter a number please: "))
@@ -28,4 +24,3 @@ def game(secret_num):
         else:
             print(f"It took you {times} {att} in total...")
             break
-    # ifyou reach here, it means you ran out of turn
