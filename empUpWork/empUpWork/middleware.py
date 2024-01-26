96200f852fb9e254a3b7bbb7527059c8723f1f50
Maxmaraimo <ozodbekmahmaraimov16@gmail.com>
2024-01-26 11:06:04 +0500

    middleware.py

--- empUpWork/empUpWork/middleware.py
+++ empUpWork/empUpWork/middleware.py
@@ -12,15 +12,12 @@ class MyMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        # Get the view function name
 
-        # Code to be executed for each request before
         print('I am before the view is called')
 
         request.start_time = time.time()
         response = self.get_response(request)
         response = print_time_taken(request, response)
-        # Do something after the view is called
 
         view_name = request.resolver_match.view_name
         if view_name is not None:
